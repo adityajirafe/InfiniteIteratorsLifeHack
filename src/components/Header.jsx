ccc427982731f475fb931b2306d66440b1a09d29
Stanley Mak <stanleymaktx@gmail.com>
2023-05-29 20:54:58 +0800

    login logout toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useGlobalContext } from "../context";
 
 function Header() {
+  const { email } = useGlobalContext();
   return (
     <AppBar position="static" sx={{ width: "100vw", backgroundColor: (theme) => theme.palette.primary.main  }}>
       <Toolbar>
@@ -21,12 +23,20 @@ function Header() {
         <Button color="inherit" component={Link} to="/map">
           Map
         </Button>
+        {!email ? (
+        <>
         <Button color="inherit" component={Link} to="/login">
           Login
         </Button>
         <Button color="inherit" component={Link} to="/signup">
           Signup
         </Button>
+        </>
+        ) : (
+          <Button color="inherit" component={Link} to="/home">
+          Logout
+        </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
