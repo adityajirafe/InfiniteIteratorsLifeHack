4a7bb7016d2f3710091e277950c02af95c7e81b5
adityajirafe <aditya.jirafe@u.nus.edu>
2023-05-30 02:56:22 +0800

    route for matches

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,21 @@ import { Link } from "react-router-dom";
 import { useGlobalContext } from "../context";
 
 function Header() {
-  const { email, setEmail, address, setAddress, township, setTownship } = useGlobalContext();
+  const { email, setEmail, address, setAddress, township, setTownship } =
+    useGlobalContext();
   const onLogout = () => {
     setEmail("");
     setAddress("");
     setTownship("");
-  }
+  };
   return (
-    <AppBar position="static" sx={{ width: "100vw", backgroundColor: (theme) => theme.palette.primary.main  }}>
+    <AppBar
+      position="static"
+      sx={{
+        width: "100vw",
+        backgroundColor: (theme) => theme.palette.primary.main,
+      }}
+    >
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Infinite Carpool Iterators
@@ -19,28 +26,33 @@ function Header() {
         <Button color="inherit" component={Link} to="/home">
           Home
         </Button>
-        <Button color="inherit" component={Link} to="/requests">
-          Requests
+        <Button color="inherit" component={Link} to="/neighbours">
+          Neighbours
         </Button>
-        <Button color="inherit" component={Link} to="/groupings">
-          Groupings
+        <Button color="inherit" component={Link} to="/matched">
+          Matched
         </Button>
         <Button color="inherit" component={Link} to="/map">
           Map
         </Button>
         {!email ? (
-        <>
-        <Button color="inherit" component={Link} to="/login">
-          Login
-        </Button>
-        <Button color="inherit" component={Link} to="/signup">
-          Signup
-        </Button>
-        </>
+          <>
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={Link} to="/signup">
+              Signup
+            </Button>
+          </>
         ) : (
-        <Button color="inherit" component={Link} to="/login" onClick={onLogout}>
-          Logout
-        </Button>
+          <Button
+            color="inherit"
+            component={Link}
+            to="/login"
+            onClick={onLogout}
+          >
+            Logout
+          </Button>
         )}
       </Toolbar>
     </AppBar>
